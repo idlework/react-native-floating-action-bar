dd30069776ed3215fbb1d08c126cb62ce0e998f1
Tim Broddin <tim@wannabes.be>
2020-04-11 21:07:52 +0200

    Update FloatingActionBar.js
    add containerStyle prop

--- src/FloatingActionBar.js
+++ src/FloatingActionBar.js
@@ -11,12 +11,13 @@ const FloatingActionBar = ({
   position,
   selectedIndex,
   style,
+  containerStyle
 }) => {
   const [currentIndex, setCurrentIndex] = useState(selectedIndex || 0);
   const selectedItem = items[selectedIndex];
   const size = getSize(position);
   return (
-    <View style={[styles.container, getPositions(position, offset)]}>
+    <View style={[styles.container, getPositions(position, offset), containerStyle ? containerStyle : {}]}>
       <View style={[styles.content, style]}>
         <View style={[getDirection(position)]}>
           <FloatingActionIndicator
